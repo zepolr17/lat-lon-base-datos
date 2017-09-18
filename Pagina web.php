38ac32e0beca13ee057f90a0ec8380068d94ab09
Vivi <Vivi@Viviana>
2017-09-18 09:34:22 -0500

    Pagina web

---
+++ Pagina web.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <form action="procesador.php">
+            <p>lat<p>
+                <input type="text" name="lat">
+                <br>
+                 <p>lon<p>
+                <input type="text" name="lon">
+                <br>
+                <input type="submit">
+        </form>
+    </body>
+</html>
+
